3ca53eb1b6b6e86a2202c0dd9e78bdf7b4f36219
kdog3682 <kdog3682@gmail.com>
2020-05-08 22:09:25 -0400

    asdf

---
+++ test.py
@@ -0,0 +1,74 @@
+defcompilefffjj;
+store = {}
+storeqwdatepo = os.path.getmtimefffjj
+storeqwdatestamppo =datestampffdn
+preset =qw win correct successconfig plan todo placeholder po
+lettermapdn
+lettermap =qww
+    oobservations
+    c code
+    n new
+    td todo
+    sc student circles
+    cb challengebasketball
+    
+    r reminders
+    e event
+    me self
+    
+poo
+for line in readfffjj.splitffnjjj;
+text = matchAndReplacefftextjj
+//text = readfffjj
+text = matchAndReplacefftext, listingjj
+uplisting = qw
+// this is not correct.
+delete up until for line
+fordn
+read textdn
+read file
+text = replace dotalldndollar '' textgm dn
+text = replaceff'dotalldndollar', '', text, 'gm'jj
+delete until for line
+delete up until for line
+delete up until for line
+delete up until for line
+text = readfffjj
+for line in text;
+if text.endswith'dn' continue
+delete up
+if line.endswith'dn' continue
+ifline[zero;one] equals dwl indn
+ddddddafdsfsdffffdn
+first = matchff'caretwplu,line, 'gm'jjif first in preset:
+    storeqwfirstp =body
+up tag, body =findallff'ffwplusjj ffdotalljj
+if not store[tag] store[tag] = array
+store[tag].appendfflinedwlbodyjj
+if tag in letters.keysffjj;
+tag = lettersqwtagpo
+
+33 repeat the pattern of the if notif lengthfftagjj less than 3
+tag = tag
+else tag is none
+if tag:
+    do the thing
+    nexttotext = replace: text = multipleReplacefftext,caretplus','dotalldnslashn
+    return store
+    33 the store represents a single day's worth of notes. it ignores fff jjj stuff.
+    
+    
+    sdfsd
+    sdfsdf
+    sdfsdfs:
+        sdfsdf
+    sdfsd
+    sdfdsfsd
+    sdfsdf sdlkfjsdlfk
+    qsdfs
+    df
+    sdf
+    sdfsdfsd {
+        sdfsdfsdfsdsf
+            
+    }
